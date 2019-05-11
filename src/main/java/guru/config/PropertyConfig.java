90704c4f60d5cf554238da50598014a6eb1112d1
sanya <ambruzs.istvan@gmail.com>
2019-05-11 14:22:45 +0200

    multiple properties files

--- src/main/java/guru/config/PropertyConfig.java
+++ src/main/java/guru/config/PropertyConfig.java
@@ -2,25 +2,13 @@ package guru.config;
 
 import guru.examplebeans.FakeDataSource;
 import guru.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment environment;
 
     @Value("${guru.username}")
     String user;
@@ -59,10 +47,13 @@ public class PropertyConfig {
         return fakeDataSource;
     }
 
-
+/*
+    if we're using application.properties, this is unnecessary
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties() {
         PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
         return propertySourcesPlaceholderConfigurer;
     }
+
+ */
 }
